57cf5681a811db7270bf3d983b4d19e4e416165e
Akinbinu Arike <akinbinuarike2020@gmail.com>
2022-08-15 19:08:46 -0700

    first commit

--- src/Register.js
+++ src/Register.js
@@ -2,89 +2,142 @@ import React from "react";
 import { useState } from "react";
 const Register = () => {
   // States for registration
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   //States for checking the errors
   const [submitted, setSubmitted] = useState(false);
-const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
+  const [cPasswordError, setCPasswordError] = useState(false);
+  
+  
   //Handling the name change
+ const handleName = (e) => {
+    setFirstName(e.target.value);
+    setSubmitted(false);
+  };
 
-  const handleName = (e) => {
-    setName(e.target.value);
+  //Handling the last name change
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
     setSubmitted(false);
   };
 
   //Handling the email change
-
   const handleEmail = (e) => {
     setEmail(e.target.value);
     setSubmitted(false);
   };
 
   // Handling the password change
-
   const handlePassword = (e) => {
     setPassword(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the confirm password change
+  const handleConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
     setSubmitted(false);
   };
 
   //Handling the form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name === "" || email === "" || password === "") {
+     if (password !== confirmPassword ) {
+    return   setCPasswordError(true);
+    }
+
+    if (
+      firstName === "" ||
+      lastName === "" ||
+      email === "" ||
+      password === "" ||
+      confirmPassword ===""
+    ) {
       setError(true);
     } else {
       setSubmitted(true);
-      setError(false);
-    }
+      setError(false);  
+      setCPasswordError(false);
+    } 
+   
   };
 
-
   // Showing success message
-  const successMessage =() => {
+  const successMessage = () => {
     return (
-      <div className="success" style={{
-        display: submitted ? '' : 'none',
-      }}>
-        <h3>User {name} successfully registered!</h3>
+      <div
+        className="success"
+        style={{
+          display: submitted ? "" : "none",
+        }}
+      >
+        <h3>User successfully registered!</h3>
       </div>
-    )
-  }
+    );
+  };
 
   // Showing error message
-  const errorMessage =() => {
+  const errorMessage = () => {
     return (
-      <div className="error" style={{
-        display: error ? '' : 'none',
-      }}>
+      <div
+        className="error"
+        style={{
+          display: error ? "" : "none",
+        }}
+      >
         <p>Please enter all the fields</p>
       </div>
-    )
-  }
+    );
+  };
 
+  // Showing error confirm password
+  const errorPassword = () =>{
+return (
+  <div className="errorPwrd" style={{
+    display: cPasswordError ? "" : "none",
+  }} >
+<p>Confirm Password</p>
+  </div>
+)
+
+  }
   return (
     <div className="form">
       <div>
         <h1>Registration Form</h1>
       </div>
 
+      {/* Calling to the methods */}
 
-{/* Calling to the methods */}
-
-<div className="messages">
-  {errorMessage()}
-  {successMessage()}
-</div>
+      <div className="messages">
+        {errorMessage()}
+        {successMessage()}
+       
+      </div>
       <form>
         <label className="label">
-          Name:
+          First Name:
           <input
             onChange={handleName}
             className="input"
             type="text"
-            value={name}
+            placeholder="Enter First Name"
+            value={firstName}
+          />
+        </label>
+        <label className="label">
+          Last Name:
+          <input
+            onChange={handleLastName}
+            className="input"
+            type="text"
+            placeholder="Enter Last Name"
+            value={lastName}
           />
         </label>
         <label className="label">
@@ -93,6 +146,7 @@ const [error, setError] = useState(false)
             onChange={handleEmail}
             className="input"
             type="text"
+            placeholder="Enter Email"
             value={email}
           />
         </label>
@@ -102,11 +156,28 @@ const [error, setError] = useState(false)
             onChange={handlePassword}
             className="input"
             type="text"
+            pattern="[a-zA-Z0-9-]+" required
+            placeholder="Enter Password"
             value={password}
           />
         </label>
+        <label className="label">
+          Confirm password:
+          <input
+            onChange={handleConfirmPassword}
+            className="input"
+            type="text"
+            pattern="[a-zA-Z0-9-]+ " required
+            placeholder="Enter Confirm Password"
+            value={confirmPassword}
+           
+          />
+            {errorPassword()}
+        </label>
 
-        <button onClick={handleSubmit} className="btn" type="submit">Submit</button>
+        <button onClick={handleSubmit} className="btn" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
